fe5e053fd8de4a77a2adbfb4163ed3373167ee90
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-08-01 20:50:24 +0600

    Update welcome.blade.php
    welcome blade modified

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,21 @@
 @extends('layout.master')
-
+@extends('layout.header')
 @section('content')
-<h1>Hello , all ok</h1>
+    <div class="container">
+        <div class="row">
+            <div class="card">
+                <div>
+                    <div class="card-header">
+                        Home
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
+@extends('layout.footer')
