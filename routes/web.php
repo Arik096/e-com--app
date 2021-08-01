96a581b45dcb5cd96423cad593cad851dee5aeb1
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-08-01 20:50:34 +0600

    Update web.php
    login page route added

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::view('/login', 'user.login')->name('loginPage');
