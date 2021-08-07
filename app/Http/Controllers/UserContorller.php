7229819c72b1abbe6cdb5b9224aa8181606b00d4
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-08-07 19:54:05 +0600

    Create UserContorller.php
    login function added

---
+++ app/Http/Controllers/UserContorller.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+
+class UserContorller extends Controller
+{
+    protected function login(Request $request)
+    {
+        // return DB::table('users')
+        //     ->where('email', $request->email)
+        //     ->get();
+
+        $user = User::where('email', $request->email)
+            ->get();
+
+        if ($user && Hash::check($request->password, $user->password)) {
+            $request->session()->put('user', $user);
+            return redirect(route('home'));
+        } else {
+            return "Username & Password Doesn't Match";
+        }
+    }
+}
