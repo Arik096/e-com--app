7192cc84fa975db6275aa0714e5433560fe15b5c
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-08-12 19:09:03 +0600

    Update master.blade.php
    <livewire:styles />
    ...
    <livewire:scripts />

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <title>E-Commerce</title>
+     @livewireStyles
 </head>
 <body>
 
@@ -19,5 +20,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 @yield('footer')
+@livewireScripts
 </body>
 </html>
